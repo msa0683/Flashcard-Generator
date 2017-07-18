13950aa6a291bf9f14f95cee99ebb5bb1e81e18b
msa0683 <msa0683@yahoo.com>
2017-07-17 19:17:09 -0700

    first push

---
+++ ClozeCard.js
@@ -0,0 +1,16 @@
+[
+  {“question”:“Who was the first president of the United States?“,
+  “answer”:“George Washington”},
+
+ {“question”:“Who was the 44rd president of the United States?“,
+  “answer”:“Barack Obama”},
+
+ {“question”:“Who was the 44rd president of the United States?“,
+  “answer”:“Barack Obama”},
+
+ {“question”:“What is the capital of Australia?“,
+  “answer”:“Canberra”},
+
+ {“question”:“What is the only manmade object that is observable from the moon?“,
+  “answer”:“Great Wall of China”}
+]
